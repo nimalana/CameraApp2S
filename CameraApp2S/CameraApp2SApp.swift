f52fee9b3da530752b974923ad1f10cfcfaf1768
Nimalan Arulvelan <nimalan.arulvelan@gmail.com>
2026-03-15 18:14:46 -0400

    Initial Commit

---
+++ CameraApp2S/CameraApp2SApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraApp2SApp.swift
+//  CameraApp2S
+//
+//  Created by Nimalan Arulvelan on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct CameraApp2SApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CameraApp2S/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CameraApp2S
+//
+//  Created by Nimalan Arulvelan on 3/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
